e1cb5c667b146c9275d7e0a81170753b66936c40
ahadg <48055296+ahadg@users.noreply.github.com>
2020-09-15 15:18:36 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/complainsystem').then(console.log('Connection was successful'));
+try {
+    mongoose.connect(
+        //'mongodb://localhost:27017/complainsystem', 
+    'mongodb+srv://root:ahad1234@mycluster.ruutg.mongodb.net/complainsystem?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(console.log('Connection was successful'));    
+} catch (error) {
+    console.log(error)
+}
+//mongoose.connect('mongodb://localhost:27017/complainsystem').then(console.log('Connection was successful'));
